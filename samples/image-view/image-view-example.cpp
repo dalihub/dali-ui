dalihub/dali-demo

Pass BaseHandle signal arguments by value
Change-Id: Idd9a0536e19ebddfe2eb098d51131347be047c45

examples/image-view/image-view-example.cpp
@@ -108,7 +108,7 @@ public:
     // Nothing to do here
   }
 
-  void Create(Application& application)
+  void Create(Application application)
   {
     // The Init signal is received once (only) during the Application lifetime
 
@@ -286,7 +286,7 @@ private:
   /**
    * Main key event handler
    */
-  void OnKeyEvent(const KeyEvent& event)
+  void OnKeyEvent(KeyEvent event)
   {
     if(event.GetState() == KeyEvent::DOWN)
     {

examples/color-visual/color-visual-example.cpp
@@ -71,7 +71,7 @@ public:
   }
 
   // The Init signal is received once (only) during the Application lifetime
-  void Create(Application& application)
+  void Create(Application application)
   {
     // Get a handle to the window
     Window window = application.GetWindow();
@@ -91,7 +91,7 @@ public:
     window.KeyEventSignal().Connect(this, &ColorVisualExample::OnKeyEvent);
   }
 
-  bool OnTouch(Actor actor, const TouchEvent& touch)
+  bool OnTouch(Actor actor, TouchEvent touch)
   {
     if(touch.GetState(0) == PointState::UP)
     {
@@ -128,7 +128,7 @@ public:
     return true;
   }
 
-  void OnKeyEvent(const KeyEvent& event)
+  void OnKeyEvent(KeyEvent event)
   {
     if(event.GetState() == KeyEvent::DOWN)
     {

examples/web-view/web-view-example.cpp
@@ -59,7 +59,7 @@ public:
   }
 
   // The Init signal is received once (only) during the Application lifetime
-  void Create(Application& application)
+  void Create(Application application)
   {
     // Get a handle to the window
     Window window = application.GetWindow();
@@ -104,7 +104,7 @@ public:
     mAddressLabel.SetProperty(Toolkit::TextLabel::Property::TEXT, url.c_str());
   }
 
-  bool OnTouchText(Actor actor, const TouchEvent& touch)
+  bool OnTouchText(Actor actor, TouchEvent touch)
   {
     if(touch.GetState(0) == PointState::UP)
     {
@@ -116,7 +116,7 @@ public:
     return true;
   }
 
-  void OnKeyEvent(const KeyEvent& event)
+  void OnKeyEvent(KeyEvent event)
   {
     if(event.GetState() == KeyEvent::DOWN)
     {

dalihub/dali-ui

samples/image-view/image-view-example.cpp
@@ -36,7 +36,7 @@ public:
   }
 
 private:
-  void OnInit(Application& application)
+  void OnInit(Application application)
   {
     Window window     = application.GetWindow();
     auto   windowSize = window.GetSize();
@@ -64,7 +64,7 @@ private:
                           static_cast<int>(imageView.GetLoadingStatus()));
   }
 
-  void OnKeyEvent(const KeyEvent& event)
+  void OnKeyEvent(KeyEvent event)
   {
     if(event.GetState() == KeyEvent::DOWN)
     {

samples/visual-base/color-visual-example.cpp
@@ -61,7 +61,7 @@ public:
   }
 
 private:
-  void OnInit(Application& application)
+  void OnInit(Application application)
   {
     Window window = application.GetWindow();
     window.SetBackgroundColor(UiColor(0xFFFFFF));
@@ -192,7 +192,7 @@ private:
                 .SetRequestedHeight(50_spx)
                 .SetLayoutParams(StackLayoutParams::New().SetWeight(0.5f))
                 .AsInteractive([this](InteractiveTrait& trait) {
-                  trait.ClickedSignal().Connect(this, [this](View, const InputEvent&) -> bool
+                  trait.ClickedSignal().Connect(this, [this](View, InputEvent) -> bool
                   {
                     PushVisual();
                     return true;
@@ -214,7 +214,7 @@ private:
                 .SetRequestedHeight(50_spx)
                 .SetLayoutParams(StackLayoutParams::New().SetWeight(0.5f))
                 .AsInteractive([this](InteractiveTrait& trait) {
-                  trait.ClickedSignal().Connect(this, [this](View, const InputEvent&) -> bool
+                  trait.ClickedSignal().Connect(this, [this](View, InputEvent) -> bool
                   {
                     PopVisual();
                     return true;
@@ -282,7 +282,7 @@ private:
     mVisualCounter.SetText((std::string("Visuals Count : #") + numberOfVisuals).c_str());
   }
 
-  void OnKeyEvent(const KeyEvent& event)
+  void OnKeyEvent(KeyEvent event)
   {
     if(event.GetState() != KeyEvent::UP)
     {

samples/web-view/web-view-example.cpp
@@ -75,7 +75,7 @@ private:
   // -----------------------------------------------------------------------
   // Init
   // -----------------------------------------------------------------------
-  void OnInit(Application& application)
+  void OnInit(Application application)
   {
     Window window     = application.GetWindow();
     auto   windowSize = window.GetSize();
@@ -155,7 +155,7 @@ private:
   // -----------------------------------------------------------------------
   // Key input
   // -----------------------------------------------------------------------
-  void OnKeyEvent(const KeyEvent& event)
+  void OnKeyEvent(KeyEvent event)
   {
     if(event.GetState() != KeyEvent::DOWN)
     {