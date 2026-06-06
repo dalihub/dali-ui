f5b3543deea17f1b2ef22740cfaca5b18ac39be8
홍은기/Tizen Platform Lab(SR)/삼성전자 <eunkiki.hong@samsung.com>
2026-06-06 11:29:52 +0900

    Update test-actor-utils.h

--- automated-tests/src/dali-test-suite-utils/test-actor-utils.h
+++ automated-tests/src/dali-test-suite-utils/test-actor-utils.h
@@ -2,7 +2,7 @@
 #define DALI_TEST_ACTOR_UTILS_H
 
 /*
- * Copyright (c) 2023 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2026 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,6 @@
 namespace Dali
 {
 class Actor;
-class Image;
 class Texture;
 
 /**
